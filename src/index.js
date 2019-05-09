d7263c3804e6474d2634a49108b625f46d3a4e38
Rick <richelton14@gmail.com>
2019-05-09 11:32:57 -0400

    Testando a rota '/show/:id' da api

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ window.search = search;
 
 // campina grande: geocode=-7.230677,-35.881639,1mi
 async function search() {
-    // let response = await axios.get("http://localhost:3001/search/campina?count=5&geocode=-7.230677,-35.881639,1mi");
     
+    // Buscando os tweets
+    /*
+    let response = await axios.get("http://localhost:3001/search/campina?count=5&geocode=-7.230677,-35.881639,1mi");
+    let dados = response.data;
+    console.log(dados);
+    */
+    
+    // Pegando link do meu tweet.
     let response = await axios.get("http://localhost:3001/show/1126443732839088130");
     let dados = response.data;
+    
+    let lista = dados['text'].split(" ");
+    console.log(lista[lista.length-1]);
 
-    console.log(dados);
 }
