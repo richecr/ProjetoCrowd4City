53da7abe557f73a157edd319f42206b7100c8747
Rick <richelton14@gmail.com>
2019-06-06 15:42:04 -0400

    Bug para ser corrigido: Refazer o carregamento dos tweets para virem com a key 'full_text'

--- twitter_scraping/get_metadata.py
+++ twitter_scraping/get_metadata.py
@@ -20,8 +20,8 @@ auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
 auth.set_access_token(keys['access_token'], keys['access_token_secret'])
 api = tweepy.API(auth)
 user = user.lower()
-output_file = 'dados/seguranca/{}.json'.format(user)
-output_file_short = 'dados/seguranca/{}_short.json'.format(user)
+output_file = 'dados/educacao/{}.json'.format(user)
+output_file_short = 'dados/educacao/{}_short.json'.format(user)
 compression = zipfile.ZIP_DEFLATED
 
 with open('all_ids.json') as f:
@@ -41,7 +41,7 @@ for go in range(i):
     id_batch = ids[start:end]
     start += 100
     end += 100
-    tweets = api.statuses_lookup(id_batch, tweet_mode=True)
+    tweets = api.statuses_lookup(id_batch, tweet_mode="extended")
     for tweet in tweets:
         all_data.append(dict(tweet._json))
 
@@ -51,7 +51,7 @@ with open(output_file, 'w') as outfile:
     json.dump(all_data, outfile)
 
 print('creating ziped master json file')
-zf = zipfile.ZipFile('dados/seguranca/{}.zip'.format(user), mode='w')
+zf = zipfile.ZipFile('dados/educacao/{}.zip'.format(user), mode='w')
 zf.write(output_file, compress_type=compression)
 zf.close()
 
@@ -90,7 +90,7 @@ with open(output_file_short, encoding="utf-8") as master_file:
     data = json.load(master_file)
     fields = ["favorite_count", "source", "full_text", "in_reply_to_screen_name", "is_retweet", "created_at", "retweet_count", "id_str", "coordinates"]
     print('creating CSV version of minimized json master file')
-    f = csv.writer(open('dados/seguranca/{}.csv'.format(user), 'w'))
+    f = csv.writer(open('dados/educacao/{}.csv'.format(user), 'w'))
     f.writerow(fields)
     for x in data:
         f.writerow([x["favorite_count"], x["source"], x["full_text"].encode("utf-8"), x["in_reply_to_screen_name"], x["is_retweet"], x["created_at"], x["retweet_count"], x["id_str"], x["coordinates"]])
