116cb4aa3ba91d464967d658dff1546a51bb811b
Rick <richelton14@gmail.com>
2019-06-25 09:54:59 -0400

    Analisando Tweets para testes

--- Classificador/Main.py
+++ Classificador/Main.py
@@ -1,4 +1,11 @@
+import pandas as pd
+tweets = pd.read_csv("../twitter_scraping/tweets_testes.csv", sep='\t', )
+print(tweets.head())
+
+
+'''
 with open("../twitter_scraping/tweets_testes.csv", "r", encoding="utf-8") as file:
     texto = file.read()
     
-    print(file.read().split(",")[1])
+    print(texto)
+'''
