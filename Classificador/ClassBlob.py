20054fe0f1091aa79d2a8cb3dd949fcc8b009c7b
Rick <richelton14@gmail.com>
2019-07-02 09:50:41 -0400

    TextBlob

--- Classificador/ClassBlob.py
+++ Classificador/ClassBlob.py
@@ -4,6 +4,7 @@ import pandas as pd
 tweets = pd.read_csv("tweets.csv", encoding='utf-8')
 
 def preprocessa(tweets, reclamacao):
+    pass
     # TODO
 
-c = NaiveBayesClassifier(tweets['full_text'])
+c = NaiveBayesClassifier(preprocessa(tweets['full_text'], tweets['reclamacao']))
