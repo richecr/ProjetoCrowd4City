ae6f6821034c00932837469c25462b0a45476d83
Rick <richelton14@gmail.com>
2019-07-04 09:02:32 -0400

    limpando os tweets: Aumentando a acurácia do Classificador

--- Classificador/ClassBlob.py
+++ Classificador/ClassBlob.py
@@ -1,10 +1,26 @@
+from textblob import TextBlob
 from textblob.classifiers import NaiveBayesClassifier
 import pandas as pd
 
 tweets = pd.read_csv("tweets.csv", encoding='utf-8')
+testes = [
+    ("Gostaria de pedir uma ajuda. A Escola Municipal Érico Verissimo que fica localizada no bairro Fazenda Botafogo  perto de Acari. Os alunos estão sem aula e ficaram até sexta porque marginais roubaram os fios da escola ela 4° vez esse ano","sim"),
+    ("Rua joao sergio com lixo em toda parte","sim"),
+    ("escola na rua são josé está sem funcionar","sim"),
+    ("escola sem aula","sim"),
+    ("Escola municipal do bairro mangabeira esta sem aulas ha mais de semanas","sim"),
+    ("pessoas são um lixo","não"),
+    ("ALUNOS SEM AULA POR FALTA DE ENERGIA País de alunos da escola municipal Rubens Machado no bairro Vale Verde, aguardam conserto do transformador para que a escola volte a ter aulas normalmente. A resposta que... https://t.co/GRXA2Sv3Lw","sim")
+]
 
 def preprocessa(tweets, reclamacao):
-    pass
-    # TODO
+    saida = []
+    for i in range(len(tweets)):
+        saida.append((tweets[i], reclamacao[i]))
 
-c = NaiveBayesClassifier(preprocessa(tweets['full_text'], tweets['reclamacao']))
+    return saida
+
+tw = preprocessa(tweets['full_text'], tweets['reclamacao'])
+c = NaiveBayesClassifier(tw)
+
+print(c.accuracy(testes))
--- Classificador/Main.py
+++ Classificador/Main.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import pandas as pd
 from sklearn import metrics
@@ -9,9 +10,17 @@ from sklearn.model_selection import cross_val_score, cross_val_predict, train_te
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
+# Limpa os tweets: retirando marcações, links.
+def limpa_dados(instancia):
+    instancia = re.sub(r"http\S+", "", instancia).lower().replace('.','').replace(';','').replace('-','').replace(':','').replace(')','')
+    return (instancia)
+
 # Carregando os arquivos.
 tweets = pd.read_csv("tweets.csv", encoding='utf-8')
 tests = pd.read_csv("testes.csv", encoding='utf-8')
+
+tweets_limpos = [limpa_dados(i) for i in tweets['full_text']]
+ttweets_testes_limpos = [limpa_dados(i) for i in tests['full_text']]
 
 # Criando o classificador.
 pip_simples = Pipeline([
@@ -23,12 +32,12 @@ pip_simples = Pipeline([
 ])
 
 # treinando.
-pip_simples.fit(tweets['full_text'], tweets['reclamacao'])
+pip_simples.fit(tweets_limpos, tweets['reclamacao'])
 
 # Função para avaliar a eficácia do modelo. 
 def avaliaModelo():
     # Validando os Modelos com Validação Cruzada
-    resultados = cross_val_predict(pip_simples, tweets['full_text'], tweets['reclamacao'], cv=10)
+    resultados = cross_val_predict(pip_simples, tweets_limpos, tweets['reclamacao'], cv=10)
     # Medindo a acurácia média do modelo.
     print(metrics.accuracy_score(tweets['reclamacao'], resultados))
     reclamacao1 = ['não','sim']
@@ -38,7 +47,7 @@ def avaliaModelo():
 print(avaliaModelo())
 
 # Testando o modelo.
-predicted = pip_simples.predict(tests['full_text'])
+predicted = pip_simples.predict(ttweets_testes_limpos)
 print(predicted)
 
 # Precisão do modelo.
