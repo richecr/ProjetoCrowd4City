42e401924dc0b87075571b415b7065521eda52ff
Rick <richelton14@gmail.com>
2019-07-16 10:01:32 -0400

    Melhorando código de Clas2.py

--- Classificador/treinos.py
+++ Classificador/treinos.py
@@ -24,7 +24,7 @@ def acharIndiceTweet(tweet_text):
             cont += 1
     return cont
 
-indice = acharIndiceTweet("")
+indice = acharIndiceTweet("maior silêncio aqui na rua, e o som tá alto dms aqui no terraço")
 
 cont = 0
 for t in fileR.itertuples(0):
