1070379a74489c2551294631b00885a9a82b0680
Rick Elton <richelton14@gmail.com>
2019-07-29 17:48:45 -0300

    Raspagem de dados: - Capturando os links dos vídeos - Salvando links em um arquivo json

--- scraping_jpb/teste.py
+++ scraping_jpb/teste.py
@@ -3,29 +3,44 @@ from __future__ import unicode_literals
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from time import sleep
 import youtube_dl
 import json
+import csv
 
 driver = webdriver.Firefox()
 driver.get("http://g1.globo.com/busca/?q=calendario+jpb&page=1&order=recent&species=v%C3%ADdeos")
 cont  = 0
 found_noticias = driver.find_elements_by_css_selector(".results__list")
-# noticias = found_noticias[0]
+n = found_noticias[0].find_elements_by_tag_name("li")
+quantidade = 60
 
-n = found_noticias[0].find_elements_by_css_selector(".widget widget--card widget--info")
+while (len(n) < quantidade):
+    sleep(1)
+    div_next_page = driver.find_elements_by_css_selector(".pagination")
+    next_page = div_next_page[0].find_elements_by_tag_name("a")
+    found_noticias = driver.find_elements_by_css_selector(".results__list")
+    n = found_noticias[0].find_elements_by_tag_name("li")
+    next_page[0].click()
 
+links = []
 for noticia in n:
     div = noticia.find_elements_by_css_selector(".widget--info__text-container")
-    a = div.find_elements_by_css_selector("a")
+    a = div[0].find_elements_by_tag_name("a")
 
-    print(a.get_attribute("href"))
+    links.append(a[0].get_attribute("href"))
 
+print(len(links))
+# write to json
+with open("./links.json", 'w') as outfile:
+    json.dump(links, outfile)
+
+driver.close()
 
 '''
 while (cont < 6):    
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 '''
-
 '''
 def my_hook(d):
     if d['status'] == 'finished':
@@ -38,5 +53,5 @@ ydl_opts = {
 }
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://globoplay.globo.com/v/7780021/'])
+    ydl.download(['https://globoplay.globo.com/v/7801566/'])
 '''
