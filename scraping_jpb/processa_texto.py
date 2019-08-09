b987c7d053c672bfc8d6ef82cfd0c87e5da8005c
Rick <richelton14@gmail.com>
2019-08-09 11:09:13 -0400

    Geocoding: Utilizando a biblioteca geocoder com arcgis

--- scraping_jpb/processa_texto.py
+++ scraping_jpb/processa_texto.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 import requests
 import json
+import geocoder
 
 textos_limpos = []
 arq = csv.DictReader(open("./textos_videos.csv", encoding='utf-8'))
@@ -16,6 +17,7 @@ print(textos_limpos)
 
 # Removendo stop words
 txts = []
+'''
 nltk.download('stopwords')
 stop_words = nltk.corpus.stopwords.words('portuguese')
 for t in textos_limpos:
@@ -28,29 +30,47 @@ for t in textos_limpos:
 
 print("-----------------------\n")
 print(txts)
-
+'''
 # Carregando modelo em português.
-nlp = spacy.load('pt')
-doc = nlp(txts[0])
+nlp = spacy.load('pt_core_news_sm')
+doc = nlp(textos_limpos[0])
 
 # Salvar as entidades que foram classificadas como LOC.
 ents_loc = [entity for entity in doc.ents if entity.label_ == "LOC"]
 print(ents_loc)
 
+# Básico: Indo atras do endereço, da primeira entidade, usando a API do geocoder com arcgis.
+# Para testes # ents_loc[0] = "Rua João Sergio de almeida"
+g = geocoder.arcgis(ents_loc[0].__str__())
+end = g.json
+print(end)
+
+if (end['confidence'] >= 5):
+    if ("campina grande" in end['address'].lower()):
+        print("CORRETO")
+    else:
+        print("ERRADO, Tenta outra combinação")
+else:
+    print("ERRADO, Tenta outra combinação")
+
+
 # Básico: Indo atras do endereço, da primeira entidade, usando a API do mapbox - geocoding
-response = requests.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + ents_loc[0].__str__() + ', joão pessoa.json?access_token=pk.eyJ1IjoicmljaGVsdG9uIiwiYSI6ImNqejFrNnRkdDA1NDkzaW1samUyY2pkc2YifQ.Nl_sJiP2M1hm-gXdm7zR1w')
+'''
+response = requests.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + ents_loc[0].__str__() + ', campina grande.json?access_token=pk.eyJ1IjoicmljaGVsdG9uIiwiYSI6ImNqejFrNnRkdDA1NDkzaW1samUyY2pkc2YifQ.Nl_sJiP2M1hm-gXdm7zR1w')
 enderecos = response.json()
 
 ends = enderecos['features'][0]
 print(ends)
 
 if (ends['relevance'] >= 0.5):
-    if (ends['context'][2]['text'] == "João Pessoa"):
+    if (ends['context'][2]['text'] == "Campina Grande"):
         print("CORRETO")
     else:
         print("ERRADO, Tenta outra combinação")
 else:
     print("ERRADO, Tenta outra combinação")
+'''
+
 
 '''
 textos = pd.read_csv("./textos_videos.csv")
