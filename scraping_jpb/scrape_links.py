3a35e608b4c28a82e5930fd6e0c53cef31e98130
Rickecr <richelton14@gmail.com>
2019-08-18 16:00:39 -0300

    Capturando todos os 600 links de vídeos do CalendárioJPB

--- scraping_jpb/scrape_links.py
+++ scraping_jpb/scrape_links.py
@@ -8,11 +8,11 @@ import json
 import csv
 
 driver = webdriver.Firefox()
-driver.get("http://g1.globo.com/busca/?q=calendario+jpb&page=1&order=recent&species=v%C3%ADdeos")
+driver.get("http://g1.globo.com/busca/?q=calendario+jpb&page=1&order=recent")
 cont  = 0
 found_noticias = driver.find_elements_by_css_selector(".results__list")
 n = found_noticias[0].find_elements_by_tag_name("li")
-quantidade = 1
+quantidade = 600
 
 if (len(n) > quantidade):
     novo_n = []
@@ -23,11 +23,15 @@ if (len(n) > quantidade):
 
 while (len(n) < quantidade):
     sleep(1)
-    div_next_page = driver.find_elements_by_css_selector(".pagination")
-    next_page = div_next_page[0].find_elements_by_tag_name("a")
-    found_noticias = driver.find_elements_by_css_selector(".results__list")
-    n = found_noticias[0].find_elements_by_tag_name("li")
-    next_page[0].click()
+    try:
+        div_next_page = driver.find_elements_by_css_selector(".pagination")
+        found_noticias = driver.find_elements_by_css_selector(".results__list")
+        n = found_noticias[0].find_elements_by_tag_name("li")
+        next_page = div_next_page[0].find_elements_by_tag_name("a")
+        next_page[0].click()
+    except:
+        break
+    
 
 links = []
 for noticia in n:
