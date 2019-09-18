be13b99e2231664baecd7c18ea41ecde4052273d
Rich <richelton14@gmail.com>
2019-09-18 15:47:55 -0300

    Novos testes e ideias

--- scraping_jpb/teste.py
+++ scraping_jpb/teste.py
@@ -28,27 +28,40 @@ lista_fim = []
 for texto in dados:
     doc = nlp(texto['texto'])
     ent_loc = [e for e in doc.ents if e.label_ == "LOC" or e.label_ == "GPE"]
+    ent_loc1 = []
+    for i in ent_loc:
+        flag = True
+        for j in ent_loc1:
+            if (i.text == j.text):
+                flag = False
+                break
+        if (flag):
+            ent_loc1.append(i)
+
     entidades = {}
     entidades['entities'] = []
-    for e in ent_loc:
-        print(e.text)
+    for e in ent_loc1:
         i = retorna_indice(e.text, texto['texto'])
         i_f = i + len(e.text)
         entidades['entities'].append((i, i_f, 'LOC'))
     
     obj = (texto['texto'].lower(), entidades)
     lista_fim.append(obj)
-    break
 
-print(lista_fim)
-with open("./textos.json", 'w') as f:
+with open("./textos.json", 'w', encoding='utf-8') as f:
     json.dump(lista_fim, f)
-'''
+f.close()
 
 teste = []
-with open("./textos.json", 'r') as r:
+with open("./textos.json", 'r', encoding='utf-8') as r:
     teste = json.load(r)
+r.close()
 
-print( tuple(teste[0])[0])
+ts = open("./treinar_spacy.txt", mode="w", encoding="utf-8")
 
-'''
+for partial in teste:
+    t1 = tuple(partial)
+    ts.write(str(t1))
+    ts.write('\n')
+
+#print( tuple(teste[0])[1])
