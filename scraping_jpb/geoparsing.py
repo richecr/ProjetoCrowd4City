4a40b91a5218c1523409089e5bfc9c877ef94533
Rickecr <richelton14@gmail.com>
2019-09-22 08:39:27 -0300

    Novos testes com geoparsing

--- scraping_jpb/geoparsing.py
+++ scraping_jpb/geoparsing.py
@@ -11,63 +11,16 @@ import gensim
 from nltk.stem.porter import *
 from nltk.stem import WordNetLemmatizer
 
-from model.model.ner_model import NERModel
-from model.model.config import Config
-from nltk import word_tokenize
-from nltk import tokenize
+from googletrans import Translator
 
-import nltk
-from nltk.tokenize import sent_tokenize
-from nltk import tokenize
-import re
-from sacremoses import MosesTruecaser, MosesTokenizer
-
-# from treinar_spacy.train_ner import Truecasing
-class Truecasing():
-    def __init__(self):
-        self.nlp = spacy.load("pt_core_news_sm")
-        self.nlp.Defaults.stop_words |= {"vamos", "olha", "pois", "tudo", "coisa", "toda", "tava", "pessoal", "dessa", "resolvido", "aqui", "gente", "tá", "né", "calendário", "jpb", "agora", "voltar", "lá", "hoje", "aí", "ainda", "então", "vai", "porque", "moradores", "fazer", "prefeitura", "todo", "vamos", "problema", "fica", "ver", "tô"}
-        self.stop_words_spacy = self.nlp.Defaults.stop_words
-        self.mtr = MosesTruecaser()
-        self.mtok = MosesTokenizer(lang="en")
-        tokenized_docs = [self.mtok.tokenize(line) for line in open('./textos.txt')]
-        self.mtr.train(tokenized_docs, save_to='textos.truecasemodel')
-
-    def truecasing(self, texto):
-        texto = self.remove_stop_words(texto)
-        # texto = self.pre_processamento(texto)
-        texto = self.mtr.truecase(texto, return_str=True)
-        return self.nlp(texto)
-
-    def pre_processamento(self, texto):
-       # texto = self.remove_stop_words(texto)
-        novo_texto = ""
-        lista = ["rua", "r.", "bairro", "avenida", "av", "travessa", "trav."]
-        prox = False
-        for palavra in texto.split():
-            if (prox):
-                novo_texto += palavra[0].upper() + palavra[1:] + " "
-                prox = False
-            elif (palavra.lower() in lista):
-                novo_texto += palavra[0].upper() + palavra[1:] + " "
-                prox = True
-            else:
-                novo_texto += palavra + " "
-
-        return novo_texto.strip()
-    
-    def remove_stop_words(self, texto):
-        saida = ""
-        for palavra in texto.split():
-            if (palavra.lower() not in self.stop_words_spacy):
-                saida += palavra + " "
-        s = saida.strip()
-        return s
+import truecase
 
 
-fields = ["endereco", "texto"]
-f = csv.writer(open('./enderecos1.csv', 'w', encoding='utf-8'))
-f.writerow(fields)
+translator = Translator() 
+
+#fields = ["endereco", "texto"]
+#f = csv.writer(open('./enderecos1.csv', 'w', encoding='utf-8'))
+#f.writerow(fields)
 
 # Configurando bibliotecas e variaveis globais.
 stemmer = PorterStemmer()
@@ -132,42 +85,20 @@ def verfica(ents_loc):
     else:
         return (False, [])
 
-config = Config()
-
-# build model
-model = NERModel(config)
-model.build()
-model.restore_session(config.dir_model)
-
 continuar = "5670399"
 def main(textos, titulos):
 	cont = 0
 	cont_erros = 0
 	flag = True
 	for texto, titulo in zip(textos, titulos):
-		if (flag):			
-			# Testar '.upper()' e '.title()'
-			# E ver qual é melhor.
-			# texto = remove_stop_words(texto.upper())
+		if (flag):
 			# print(texto)
-			words = tokenize.word_tokenize(texto, language="portuguese")
-			predsTexto = model.predict(words)
-			# print(predsTexto)
-			ents_loc = []
-			texto_ = texto.split()
-			for pos in range(len(predsTexto)):
-				if (predsTexto[pos] == "B-LOCAL" or predsTexto[pos] == "I-LOCAL"):
-					ents_loc.append(texto_[pos])
+			texto_en = translator.translate(texto, src="pt", dest="en")
+			texto_en = texto_en.text
+			t = truecase.caser.get_true_case(texto_en)
 
-			print(ents_loc)
-			# Testar título com Spacy.
-			titulo = titulo.split("-")[0]
-			print(titulo)
-			doc_titulo = nlp(titulo)
-			ents_loc1 = [entity for entity in doc_titulo.ents if entity.label_ == "LOC" or entity.label_ == "GPE"]
-			
-			'''
-			print(texto)
+			texto_pt = translator.translate(t, src="en", dest="pt")
+			texto = texto_pt.text
 			doc = nlp(texto)
 			titulo = titulo.split("-")
 			doc1 = nlp(titulo[0])
@@ -183,13 +114,12 @@ def main(textos, titulos):
 
 			result = verfica(end_encontrados)
 			if result[0]:
-				f.writerow([result[1]['address'], texto])
+				# f.writerow([result[1]['address'], texto])
 				cont += 1
 			else:
 				cont_erros += 1
 			print("\n------------------------------------------------\n")
 			print(cont, " - ", cont_erros)
-			'''
 		else:
 			if (continuar.lower() in titulo.lower()):
 				flag = True
@@ -204,3 +134,17 @@ for p in arq:
 	titulos.append(p['titulo'])
 
 main(textos_limpos, titulos)
+
+
+'''
+from model.model.ner_model import NERModel
+from model.model.config import Config
+from nltk import word_tokenize
+from nltk import tokenize
+
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk import tokenize
+import re
+from sacremoses import MosesTruecaser, MosesTokenizer
+'''
